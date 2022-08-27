770c5c7f5c1eafc36fe01b472e3fde153cf09885
Kasper Timm Hansen <kaspth@gmail.com>
2022-08-27 23:22:20 +0200

    Add in sketch code

--- lib/jabroni.rb
+++ lib/jabroni.rb
@@ -4,5 +4,32 @@ require_relative "jabroni/version"
 
 module Jabroni
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def inherited(klass)
+      Dir.glob("#{Dir.pwd}/#{klass.name.downcase}/extensions/*.rb").each do |extension|
+        klass.load_extension extension
+      end
+    end
+
+    def require_extensions(*extensions)
+      extensions.each do |extension|
+        load_extension "#{Dir.pwd}/#{name.downcase}/extensions/#{extension}.rb"
+      end
+    end
+    alias require_extension require_extensions
+
+    private
+      def load_extension(extension)
+        contents = File.read extension
+
+        case
+        when contents.gsub(/^\#.*?\n+/m, "").start_with?("class #{name}")
+          require extension
+        when !$LOADED_FEATURES.include?(extension)
+          $LOADED_FEATURES << extension
+          class_eval contents, extension, 0
+        end
+      end
+  end
 end
