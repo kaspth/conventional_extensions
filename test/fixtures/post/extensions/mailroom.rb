8dd4cb14a88a44352a5dbebc71aee601dad5c9a6
Kasper Timm Hansen <kaspth@gmail.com>
2022-08-28 01:58:11 +0200

    Try to trip up the `class Post` detecting regex

--- test/fixtures/post/extensions/mailroom.rb
+++ test/fixtures/post/extensions/mailroom.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
+# This comment is just here to check that the regex detecting `class Post` can handle an extra comment
+
+# Same with the space above, this line and then the extra newlines below:
+
+
 
 class Post
   load_extension :named
