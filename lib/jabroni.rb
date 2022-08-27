bf4a7da2888db7c6d24a18ae3feb6a2585148060
Kasper Timm Hansen <kaspth@gmail.com>
2022-08-28 00:59:17 +0200

    Hoist our primary interface to the top

--- lib/jabroni.rb
+++ lib/jabroni.rb
@@ -3,6 +3,14 @@
 require_relative "jabroni/version"
 
 module Jabroni
+  def load_extensions(*extensions)
+    Loader.new(self).load(*extensions)
+  end
+  alias load_extension load_extensions
+
+  # extend Jabroni.load_on_inherited
+  def self.load_on_inherited() = LoadOnInherited
+
   autoload :Loader, "jabroni/loader"
 
   module LoadOnInherited
@@ -13,16 +21,6 @@ module Jabroni
       klass.load_extensions
     end
   end
-
-  # extend Jabroni.load_on_inherited
-  def self.load_on_inherited
-    LoadOnInherited
-  end
-
-  def load_extensions(*extensions)
-    Loader.new(self).load(*extensions)
-  end
-  alias load_extension load_extensions
 end
 
 defined?(ActiveSupport.on_load) and ActiveSupport.on_load(:active_record) { extend Jabroni.load_on_inherited }
