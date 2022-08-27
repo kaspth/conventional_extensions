c51edf83526685cf0a74d37f9e02cbb778e8b0c6
Kasper Timm Hansen <kaspth@gmail.com>
2022-08-28 01:51:37 +0200

    Extract out frozen string literal testing to own test

--- test/fixtures/post/extensions/named.rb
+++ test/fixtures/post/extensions/named.rb
@@ -1,3 +1,7 @@
 def self.named(key)
   define_method(:named_something) { key }
 end
+
+def self.thawed_boi
+  "thawed boi"
+end
--- test/jabroni/test_loader.rb
+++ test/jabroni/test_loader.rb
@@ -2,10 +2,13 @@ require "test_helper"
 
 class Jabroni::TestLoader < Minitest::Test
   def test_methods_added
-    assert_equal "cool", Post.cool
-    assert_predicate Post.cool, :frozen?
-
+    assert_equal "cool",     Post.cool
     assert_equal "mailroom", Post.new.mailroom
+  end
+
+  def test_frozen_string_literals
+    refute_predicate Post.thawed_boi, :frozen?
+    assert_predicate Post.cool, :frozen?
     assert_predicate Post.new.mailroom, :frozen?
   end
 
